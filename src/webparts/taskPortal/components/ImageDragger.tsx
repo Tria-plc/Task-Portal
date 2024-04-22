87031e60c7de2ec1cb147163f4853869bf356c4e
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2024-04-22 11:41:16 +0300

    localzation

--- src/webparts/taskPortal/components/ImageDragger.tsx
+++ src/webparts/taskPortal/components/ImageDragger.tsx
@@ -30,11 +30,11 @@ class ImageUpload extends React.Component<IImageUploadProps> {
           <Icon type="inbox" />
         </p>
         <p className="ant-upload-text">
-          Click or drag file to this area to upload
+        {localStorage.getItem("lang") === "am" ? "ለመስቀል ፋይሉን ጠቅ ያድርጉ ወይም ወደዚህ አካባቢ ይጎትቱት" : "Click or Drag File To This Area To Upload"}
         </p>
         <p className="ant-upload-hint">
-          Support for a single or bulk upload. Strictly prohibit from uploading
-          company data or other band files
+        {localStorage.getItem("lang") === "am" ? "ለአንድ ነጠላ ወይም ለጅምላ ጭነት ድጋፍ። መስቀልን በጥብቅ ይከለክላል" : "Support For a Single Or Bulk Upload. Strictly Prohibit From Uploading"}
+        {localStorage.getItem("lang") === "am" ? "የኩባንያ ውሂብ ወይም ሌላ ባንድ ፋይሎች" : "Company Data Or Other Band Files"}
         </p>
       </Dragger>
     );
